16766af8faec011917724f762c5fb26c6237507b
l0v1 <Yl0veD@Yl0veD-PC>
2014-07-28 13:54:13 +0800

    cloud_controller auto install script.

---
+++ cloud_controller_clock/cloud_controller_clock.sh
@@ -0,0 +1,307 @@
+#!/bin/bash
+
+export PATH=/var/vcap/packages/ruby/bin:$PATH
+export RUBY_PATH=/var/vcap/packages/ruby:$RUBY_PATH
+
+cfscriptdir=/home/vcap/cf-config-script
+homedir=/home/vcap
+
+CLOUD_CONTROLLER_CLOCK_CONFIG=/var/vcap/jobs/cloud_controller_clock/config
+CLOUD_CONTROLLER_CLOCK_BIN=/var/vcap/jobs/cloud_controller_clock/bin
+
+source /home/vcap/script/cloud_controller_clock/edit_cc_clock.sh
+NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
+
+indexfile=/home/vcap/script/resources/cloud_controller_clock_index.txt
+
+#------------------------ Git init --------------------------------
+if ! (which ruby); then
+    echo "Ruby is not or error setup,please install ruby......"
+    exit 1;
+fi
+
+if [ ! -d /var/vcap ]; then
+    sudo mkdir -p /var/vcap
+fi
+
+if [ ! -d $homedir/cf-release ]; then
+    pushd $homedir
+    git clone https://github.com/cloudfoundry/cf-release 
+    cd $homedir/cf-release
+    git checkout c4dfff2
+    git submodule update --init
+    popd
+fi
+
+echo "git init cloud_controller_ng"
+pushd $homedir/cf-release
+cd src/cloud_controller_ng
+git submodule update --init
+popd
+
+if [ ! -d /var/vcap/packages/cloud_controller_ng ]; then
+    mkdir -p /var/vcap/packages/cloud_controller_ng
+fi
+
+cp -a $homedir/cf-release/src/cloud_controller_ng/* /var/vcap/packages/cloud_controller_ng
+
+#------------------------ Resolve the cloud_controller_ng depdens ------
+if [ ! -d /var/vcap/packages ]; then 
+    mkdir -p /var/vcap/packages
+fi
+
+pushd /var/vcap/packages
+
+# libpq
+if [ ! -f postgresql-9.0.3.tar.gz ]; then   
+    wget http://blob.cfblob.com/rest/objects/4e4e78bca21e122004e4e8ec647a5404f306b0ec246e
+    mv 4e4e78bca21e122004e4e8ec647a5404f306b0ec246e postgresql-9.0.3.tar.gz
+fi
+
+mkdir -p /var/vcap/packages/libpq
+
+tar xzf postgresql-9.0.3.tar.gz
+
+cd postgresql-9.0.3
+
+./configure --prefix=/var/vcap/packages/libpq
+
+pushd src/bin/pg_config
+  make
+  make install
+popd
+
+cp -LR src/include /var/vcap/packages/libpq
+
+pushd src/interfaces/libpq
+  make
+  make install
+popd
+
+popd
+
+pushd /var/vcap/packages
+
+#client-mysql
+if [ ! -f client-5.1.62-rel13.3-435-Linux-x86_64.tar.gz ]; then
+    wget http://blob.cfblob.com/rest/objects/4e4e78bca21e122204e4e9863926b104fb68b259c9fc
+    mv 4e4e78bca21e122204e4e9863926b104fb68b259c9fc client-5.1.62-rel13.3-435-Linux-x86_64.tar.gz
+fi
+
+VERSION=5.1.62-rel13.3-435-Linux-x86_64
+# Percona binary Linux build - minor change
+tar zxvf client-$VERSION.tar.gz
+
+cd client-$VERSION
+for x in bin include lib; do
+  cp -a ${x} /var/vcap/packages/mysqlclient
+done
+
+popd
+
+pushd /var/vcap/packages
+#sqlite
+if [ ! -f sqlite-autoconf-3070500.tar.gz ]; then
+    wget http://blob.cfblob.com/rest/objects/4e4e78bca11e121004e4e7d511f82104f3068661ccfa
+    mv 4e4e78bca11e121004e4e7d511f82104f3068661ccfa sqlite-autoconf-3070500.tar.gz
+fi
+
+tar xzf sqlite-autoconf-3070500.tar.gz
+mkdir -p /var/vcap/packages/sqlite
+
+cd sqlite-autoconf-3070500
+
+./configure --prefix=/var/vcap/packages/sqlite
+make
+make install
+
+popd
+
+#--------------------------------- Cloud_controller_ng install -----------
+pushd /var/vcap/packages/cloud_controller_ng
+
+bundle package --all
+
+mysqlclient_dir=/var/vcap/packages/mysqlclient
+libpq_dir=/var/vcap/packages/libpq
+
+bundle config build.mysql2 --with-mysql-dir=$mysqlclient_dir --with-mysql-include=$mysqlclient_dir/include/mysql
+bundle config build.pg --with-pg-lib=$libpq_dir/lib --with-pg-include=$libpq_dir/include
+bundle config build.sqlite3 --with-sqlite3-dir=/var/vcap/packages/sqlite
+bundle install --local --deployment --without development test
+
+popd
+
+if [ ! -d $CLOUD_CONTROLLER_CLOCK_CONFIG ]; then
+    mkdir -p $CLOUD_CONTROLLER_CLOCK_CONFIG
+fi
+
+pushd $CLOUD_CONTROLLER_CLOCK_CONFIG
+#------------------------- etcd init ---------------------------------------
+source /home/vcap/script/cloud_controller_clock/etcdinit.sh
+export PATH=/home/vcap/etcdctl/bin:$PATH
+export GOPATH=/home/vcap/etcdctl
+rm -fr /home/vcap/script/resources/db_url.txt /home/vcap/script/resources/cc_base_url.txt
+
+pushd /home/vcap/
+
+git clone https://github.com/coreos/etcdctl
+git checkout 6dcd7c2e57
+git submodule update --init
+
+popd
+
+pushd /home/vcap/etcdctl
+
+./build
+
+popd
+
+#nats-urls
+rm -fr natsdirs.txt /home/vcap/script/resources/natsip.txt
+
+etcdctl ls /deployment/v1/nats-server/nats_urls >> natsdirs.txt
+
+while read urls
+do
+etcdctl get $urls >> /home/vcap/script/resources/natsip.txt
+done < natsdirs.txt
+
+if [ ! -f /home/vcap/script/resources/natsip.txt ]; then
+    echo "nats not deployment...." >> error.txt
+    echo "Dea_logging_agent is not success!"
+    exit 1
+fi
+
+#loggregator_endpoint_urls
+rm -rf /home/vcap/script/resources/loggregator_endpoint.txt
+rm -fr traffic_dirs.txt
+etcdctl ls /deployment/v1/loggregator-traffic/traffic_url >> traffic_dirs.txt
+
+while read line
+do
+    etcdctl get $line >> /home/vcap/script/resources/loggregator_endpoint.txt
+done < traffic_dirs.txt
+
+if [ ! -f /home/vcap/script/resources/loggregator_endpoint.txt ]; then
+    echo "loggregator_traffic not deployment...." >> error.txt
+    echo "Dea_logging_agent is not success!"
+    exit 1
+fi
+
+#db
+etcdctl get /deployment/v1/db >> /home/vcap/script/resources/db_url.txt
+
+#cc_base_url
+etcdctl get /deployment/v1/manifest/domain >> /home/vcap/script/resources/cc_base_url.txt
+
+#index and register
+etcdctl mkdir /deployment/v1/cloud_controller_clock
+etcdctl mkdir /deployment/v1/cloud_controller_clock/ccclock_urls
+etcdctl mkdir /deployment/v1/cloud_controller_clock/index
+
+rm -fr ccclockdirs.txt /home/vcap/script/resources/ccclock_urls.txt
+
+etcdctl ls /deployment/v1/cloud_controller_clock/ccclock_urls >> ccclockdirs.txt
+
+while read ccclock_urls
+do
+etcdctl get $ccclock_urls >> /home/vcap/script/resources/ccclock_urls.txt
+done < ccclockdirs.txt
+
+# create and register uaa_urls
+
+flag="false"
+  
+if [ "$NISE_IP_ADDRESS" != "" ]; then
+    for j in `cat /home/vcap/script/resources/ccclock_urls.txt`
+    do
+    if [ "$NISE_IP_ADDRESS" == "$j" ]
+    then
+        echo "the ip:$NISE_IP_ADDRESS is exits!"
+        flag="true"
+    fi
+    done
+    if [ "$flag" == "false" ]
+    then
+        echo $etcd_endpoint
+        curl http://$etcd_endpoint:4001/v2/keys/deployment/v1/cloud_controller_clock/ccclock_urls -XPOST -d value=$NISE_IP_ADDRESS
+
+        #register index
+        message=`curl -L http://$etcd_endpoint:4001/v2/keys/deployment/v1/cloud_controller_clock/index/0 |jq '.message' | cut -f 2 -d '"'`
+        if [ "$message" == "Key not found" ]; then
+            etcdctl set /deployment/v1/cloud_controller_clock/index/0 $NISE_IP_ADDRESS
+            echo "0" > $indexfile
+        else
+            rm -fr ccclockindexdirs.txt
+            etcdctl ls /deployment/v1/cloud_controller_clock/index >> ccclockindexdirs.txt
+            last=`sed -n '$=' ccclockindexdirs.txt`
+            new_index=`expr $last + 1`
+            etcdctl set /deployment/v1/cloud_controller_clock/index/$new_index $NISE_IP_ADDRESS
+            echo "$new_index" > $indexfile
+        fi
+    fi
+    if [ "$flag" == "true" ]
+    then
+        echo "flag is true,this is info: the ip is already regist!And other just update!"
+        #keep old index
+        rm -fr oldindex.txt
+        etcdctl ls /deployment/v1/cloud_controller_clock/index >> oldindex.txt
+        for old in `cat oldindex.txt`
+        do
+            old_urls=`etcdctl get $old`
+            if [ "$old_urls" == "$NISE_IP_ADDRESS" ]; then
+                echo "$old" |cut -f6 -d '/' > $indexfile
+            fi
+        done       
+    fi
+else
+    break  
+fi
+
+#-------------------------------- Cloud_controller_clock init ----------------
+cp -a $cfscriptdir/cloud_controller_clock/config/syslog_forwarder.conf $CLOUD_CONTROLLER_CLOCK_CONFIG
+cp -a $cfscriptdir/cloud_controller_clock/config/stacks.yml $CLOUD_CONTROLLER_CLOCK_CONFIG
+cp -a $cfscriptdir/cloud_controller_clock/config/newrelic.yml $CLOUD_CONTROLLER_CLOCK_CONFIG
+rm -fr $CLOUD_CONTROLLER_CLOCK_CONFIG/cloud_controller_ng.yml
+
+#-------------------------------- Cloud_controller_clock config -------------
+#nats-servers
+while read line
+do
+echo -e "- nats://nats:c1oudc0w@$line:4222" >> lnats.txt
+done < /home/vcap/script/resources/natsip.txt
+
+nats_servers=`more lnats.txt`
+
+#index
+index=$(cat $indexfile)
+
+#base_url
+base_url=`more /home/vcap/script/resources/cc_base_url.txt`
+
+#log_endpoint_url random !!!!
+log_endpoint_url=`awk '{a[NR]=$0}END{srand();i=int(rand()*NR+1);print a[i]}' /home/vcap/script/resources/loggregator_endpoint.txt`
+
+#db_url
+db_url=`more /home/vcap/script/resources/db_url.txt`
+
+edit_cc_clock "$NISE_IP_ADDRESS" "$nats_servers" "$index" "$base_url" "$log_endpoint_url" "$db_url"
+
+rm -fr lnats.txt
+
+popd
+
+#------------------------------- Cloud_controller_clock bin-------------------
+if [ ! -d $CLOUD_CONTROLLER_CLOCK_BIN ]; then
+    mkdir -p $CLOUD_CONTROLLER_CLOCK_BIN
+fi
+
+pushd $CLOUD_CONTROLLER_CLOCK_BIN
+
+cp -a $cfscriptdir/cloud_controller_clock/bin/* $CLOUD_CONTROLLER_CLOCK_BIN/
+chmod +x $CLOUD_CONTROLLER_CLOCK_BIN/*
+
+popd
+
+echo "Cloud_controller_clock is already installed success!"
---
+++ cloud_controller_worker/edit_cc_worker.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+
+function edit_cc_worker() {
+
+echo -e "---
+#Actually NGX host and port
+local_route: $1
+external_port: 9022
+
+pid_filename: /this/isnt/used/by/the/worker
+development_mode: false
+
+message_bus_servers:
+
+  $2
+
+
+external_protocol: http
+external_domain:
+  - api.$4.xip.io
+
+system_domain_organization: DevBox
+system_domain: $4.xip.io
+app_domains: [ $4.xip.io ]
+
+jobs:
+  global:
+    timeout_in_seconds: 14400
+  
+  
+  
+  
+  
+  
+  
+  
+
+app_events:
+  cutoff_age_in_days: 31
+
+app_usage_events:
+  cutoff_age_in_days: 31
+
+audit_events:
+  cutoff_age_in_days: 31
+
+billing_event_writing_enabled: true
+
+default_app_memory: 1024
+default_app_disk_in_mb: 1024
+maximum_app_disk_in_mb: 2048
+
+request_timeout_in_seconds: 300
+
+cc_partition: default
+
+bulk_api:
+  auth_user: bulk_api
+  auth_password: \"c1oudc0w\"
+
+nginx:
+  use_nginx: true
+  instance_socket: \"/var/vcap/sys/run/cloud_controller_ng/cloud_controller.sock\"
+
+index: $3
+name: micro_ng
+
+info:
+  name: vcap
+  build: \"2222\"
+  version: 2
+  support_address: http://support.cloudfoundry.com
+  description: Cloud Foundry sponsored by Pivotal
+
+
+directories:
+ tmpdir: /var/vcap/data/cloud_controller_ng/tmp
+
+
+logging:
+  file: /var/vcap/sys/log/cloud_controller_worker/cloud_controller_worker.log
+  
+  level: debug2
+  max_retries: 1
+
+
+loggregator:
+  router: $5:3456
+  shared_secret: c1oudc0w
+  url: wss://loggregator.$4.xip.io:443
+
+
+
+
+db: &db
+  database: postgres://ccadmin:c1oudc0w@$6:5524/ccdb
+  max_connections: 25
+  pool_timeout: 10
+  log_level: debug2
+
+
+
+
+uaa:
+  url: https://uaa.$4.xip.io
+  resource_id: cloud_controller,cloud_controller_service_permissions
+  
+  verification_key: |
+      -----BEGIN PUBLIC KEY-----
+      MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHFr+KICms+tuT1OXJwhCUmR2d
+      KVy7psa8xzElSyzqx7oJyfJ1JZyOzToj9T5SfTIq396agbHJWVfYphNahvZ/7uMX
+      qHxf+ZH9BL1gk9Y6kCnbM5R60gfwjyW1/dQPjOzn9N394zd2FJoFHwdq9Qs0wBug
+      spULZVNRxq7veq/fzwIDAQAB
+      -----END PUBLIC KEY-----
+      
+
+# App staging parameters
+staging:
+  timeout_in_seconds: 900
+  minimum_staging_memory_mb: 1024
+  minimum_staging_disk_mb: 4096
+  auth:
+    user: uploaduser
+    password: \"c1oudc0w\"
+
+maximum_health_check_timeout: 180
+
+runtimes_file: /var/vcap/jobs/cloud_controller_ng/config/runtimes.yml
+stacks_file: /var/vcap/jobs/cloud_controller_worker/config/stacks.yml
+
+quota_definitions:
+  
+  default:
+    memory_limit: 10240
+    total_services: 100
+    non_basic_services_allowed: true
+    total_routes: 1000
+    trial_db_allowed: true
+  
+
+default_quota_definition: default
+
+resource_pool:
+  minimum_size: 65536
+  maximum_size: 536870912
+  resource_directory_key: cc-resources
+  
+  cdn:
+    uri: 
+    key_pair_id: 
+    private_key: \"\"
+  
+  fog_connection: {\"provider\":\"Local\",\"local_root\":\"/var/vcap/nfs/shared\"}
+
+packages:
+  app_package_directory_key: cc-packages
+  max_package_size: 1073741824
+  
+  cdn:
+    uri: 
+    key_pair_id: 
+    private_key: \"\"
+  
+  fog_connection: {\"provider\":\"Local\",\"local_root\":\"/var/vcap/nfs/shared\"}
+
+droplets:
+  droplet_directory_key: cc-droplets
+  
+  cdn:
+    uri: 
+    key_pair_id: 
+    private_key: \"\"
+  
+  fog_connection: {\"provider\":\"Local\",\"local_root\":\"/var/vcap/nfs/shared\"}
+
+buildpacks:
+  buildpack_directory_key: cc-buildpacks
+  
+  cdn:
+    uri: 
+    key_pair_id: 
+    private_key: \"\"
+  
+  fog_connection: {\"provider\":\"Local\",\"local_root\":\"/var/vcap/nfs/shared\"}
+
+db_encryption_key: c1oudc0w
+
+tasks_disabled: false
+flapping_crash_count_threshold: 3
+
+disable_custom_buildpacks: false
+
+broker_client_timeout_seconds: 60
+
+renderer:
+  max_results_per_page: 100
+  default_results_per_page: 50
+  max_inline_relations_depth: 2
+
+
+
+diego: false
+
+
+skip_cert_verify: true
+
+
+app_bits_upload_grace_period_in_seconds: 1200
+" >> /var/vcap/jobs/cloud_controller_worker/config/cloud_controller_ng.yml
+}
+
+local_host="$1"
+nats_urls="$2"
+index="$3"
+base_url="$4"
+log_endpoint_url="$5"
+db_url="$6"
+
+edit_cc_worker "$1" "$2" "$3" "$4" "$5" "$6"
+
