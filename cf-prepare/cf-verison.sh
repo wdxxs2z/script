50fc12319d0ed2c20488c73b38c71734f780638a
l0v1 <Yl0veD@Yl0veD-PC>
2014-07-31 17:30:44 +0800

    etcdstore prepore install.

---
+++ cf-prepare/cf-verison.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export PATH=/home/vcap/etcdctl/bin:$PATH
+source /home/vcap/script/cf-prepare/etcdinit.sh
+
+pushd /home/vcap/
+
+git clone https://github.com/coreos/etcdctl
+git checkout 6dcd7c2e57
+git submodule update --init
+
+popd
+
+pushd /home/vcap/etcdctl
+
+./build
+
+popd
+
+cf_version=`etcdctl get /deployment/v1/manifest/version`
+
+pushd /home/vcap/
+
+    git clone https://github.com/cloudfoundry/cf-release 
+    cd /home/vcap/cf-release
+    git pull origin  master
+    git checkout $cf_version
+    git submodule update --init
+    
+popd
