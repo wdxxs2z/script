64065dde495dae4ffa07037efeac7cef9e88a344
wdxxs2z <243219650@qq.com>
2014-09-19 10:03:46 +0800

    Modify uaa etcd bug.

--- uaa/build.sh
+++ uaa/build.sh
@@ -8,7 +8,7 @@ homedir=/home/vcap
 
 export PATH=/home/vcap/etcdctl/bin:$PATH
 
-source /home/vcap/util/etcdinit.sh > peers.txt
+source /home/vcap/script/util/etcdinit.sh > peers.txt
 while read line
 do
     export ETCDCTL_PEERS=http://$line:4001
